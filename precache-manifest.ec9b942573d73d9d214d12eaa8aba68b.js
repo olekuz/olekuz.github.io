d6803aee3e90d2027bced54ca5448e2dbd65ef3a
ci <ci@github.com>
2020-10-05 08:44:14 +0000

    Add changes

---
+++ precache-manifest.ec9b942573d73d9d214d12eaa8aba68b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f664022ad5861cccde6fdfc1b44127e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a7878573c94783d904",
+    "url": "/static/js/2.a312431d.chunk.js"
+  },
+  {
+    "revision": "d86f9d5ee017b6fd7a92a55d652a0190",
+    "url": "/static/js/2.a312431d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a6260630dd0b053f2a",
+    "url": "/static/js/main.a1358a13.chunk.js"
+  },
+  {
+    "revision": "06487be3d7d7ac8395a0",
+    "url": "/static/js/runtime-main.d0baef61.js"
+  }
+]);
